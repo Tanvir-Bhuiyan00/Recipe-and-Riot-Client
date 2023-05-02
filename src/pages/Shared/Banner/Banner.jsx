8e8105cfd301b6671cf5b3f3adc48cdbe5b899ed
Tanvir-Bhuiyan00 <mdtanvir95925@gmail.com>
2023-05-02 06:22:50 +0600

    navbar and banner section added

---
+++ src/pages/Shared/Banner/Banner.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import banner1 from "../../../assets/image/banner1.jpg";
+import banner2 from "../../../assets/image/banner2.jpg";
+import banner3 from "../../../assets/image/banner3.jpg";
+
+import './Banner.css'
+const Banner = () => {
+  return (
+    <div>
+      <Carousel className="banner-height">
+        <Carousel.Item>
+          <img
+            className="d-block w-100 banner-img banner-height"
+            src={banner1}
+            alt="First slide"
+          />
+
+          <Carousel.Caption>
+            <h1 className="project-color-one main-font-family fw-semibold">
+              Chicken Biryani with steamed Basmati Rice
+            </h1>
+            <p className="text-font-family project-color-one">
+              By Sumon Patwari in August 23 2021
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 banner-img banner-height"
+            src={banner2}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption >
+            <h1 className="project-color-one main-font-family fw-semibold">
+            Stewed Meatballs with Tomato Sauce
+            </h1>
+            <p className="text-font-family project-color-one">
+              By Bahar Mia in April 05 2022
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 banner-img banner-height"
+            src={banner3}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption >
+            <h1 className="project-color-one main-font-family fw-semibold">
+            Spicy Instant Noodles 
+            </h1>
+            <p className="text-font-family project-color-one">
+              By Ratan Chowdhury in june 21 2022
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <div>
-      <h2>This is header</h2>
-    </div>
+    <Navbar collapseOnSelect expand="lg" >
+      <Container>
+        <Navbar.Brand className="main-font-family fs-1 fw-medium project-color-two" href="#home">Recipe & Riot</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto text-font-family fs-5 fw-medium">
+            <Nav.Link className="project-color-two" href="#features">Home</Nav.Link>
+            <Nav.Link className="project-color-two" href="#pricing">Blog</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#pricing">
+              <Image style={{ height: "40px" }} src="/vite.svg" roundedCircle />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
